25b56502d445a8aa649dc58657832de57c40663a
Bry Madrid <hi@braynm.com>
2025-10-21 15:02:31 +0800

    feat(SpendingHighlightsPage): add view all transaction in txn list footer in category section

--- src/app/pages/SpendingHighlightsPage/components/CategoriesChartTxns.tsx
+++ src/app/pages/SpendingHighlightsPage/components/CategoriesChartTxns.tsx
@@ -1,4 +1,5 @@
-import { TrendingUp } from "lucide-react"
+import { ExternalLink, TrendingUp } from "lucide-react"
+import { Link } from '@tanstack/react-router'
 import { Pie, PieChart } from "recharts"
 import {
   Card,
@@ -207,13 +208,11 @@ export function CategoriesChartTxns() {
         />
       </CardContent>
       <CardFooter className="flex-col gap-2 text-sm">
-        <div className="flex items-center gap-2 leading-none font-medium">
-          Trending up by 5.2% this month <TrendingUp className="h-4 w-4" />
-        </div>
-        <div className="text-muted-foreground leading-none">
-          Showing total visitors for the last 6 months
-        </div>
+        <Link to="/txns" className="flex items-center gap-1 underline">
+          View All Transactions
+          <ExternalLink className="w-4 h-4" />
+        </Link>
       </CardFooter>
-    </Card >
+    </Card>
   )
 }
